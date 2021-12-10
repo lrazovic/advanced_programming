8659636ee8b25964e7d0c956bc9553f469b828f4
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2021-12-10 22:35:43 +0100

    analysis: add a naive text summarization functions

--- analysis/app/main.py
+++ analysis/app/main.py
@@ -1,3 +1,58 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+
+stopwords = list(STOP_WORDS)
+nlp = spacy.load("en_core_web_sm")
+punctuation = punctuation + "\n"
+
+
+def summarize(doc, length: float = 0.3) -> str:
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    max_frequency = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / max_frequency
+    sentence_scores = {}
+    sentence_tokens = [sent for sent in doc.sents]
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+    select_length = int(len(sentence_tokens) * length)
+    summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
+    final_summary = [word.text for word in summary]
+    return "".join(final_summary)
+
+
 if __name__ == "__main__":
-    print("This is the main module of the analysis package.")
-    print("Test")
+    text = """Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling.
+    The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry.
+    The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people).
+    Since the release of the first novel, Harry Potter and the Philosopher's Stone, on 26 June 1997, the books have found immense popularity, positive reviews, and commercial success worldwide.
+    They have attracted a wide adult audience as well as younger readers and are often considered cornerstones of modern young adult literature.
+    As of February 2018, the books have sold more than 500 million copies worldwide, making them the best-selling book series in history, and have been translated into eighty languages.
+    The last four books consecutively set records as the fastest-selling books in history, with the final instalment selling roughly 2.7 million copies in the United Kingdom and 8.3 million copies in the United States within twenty-four hours of its release.
+    The series was originally published in English by two major publishers, Bloomsbury in the United Kingdom and Scholastic Press in the United States. All versions around the world are printed by Grafica Veneta in Italy.
+    A play, Harry Potter and the Cursed Child, based on a story co-written by Rowling, premiered in London on 30 July 2016 at the Palace Theatre, and its script was published by Little, Brown.
+    The original seven books were adapted into an eight-part namesake film series by Warner Bros. Pictures, which is the third-highest-grossing film series of all time as of February 2020. In 2016, the total value of the Harry Potter franchise was estimated at $25 billion, making Harry Potter one of the highest-grossing media franchises of all time.
+    A series of many genres, including fantasy, drama, coming of age, and the British school story (which includes elements of mystery, thriller, adventure, horror, and romance), the world of Harry Potter explores numerous themes and includes many cultural meanings and references.
+    According to Rowling, the main theme is death.[7] Other major themes in the series include prejudice, corruption, and madness.
+    The success of the books and films has allowed the Harry Potter franchise to expand with numerous derivative works, a travelling exhibition that premiered in Chicago in 2009, a studio tour in London that opened in 2012, a digital platform on which J. K. Rowling updates the series with new information and insight, and a pentalogy of spin-off films premiering in November 2016 with Fantastic Beasts and Where to Find Them, among many other developments.
+    Most recently, themed attractions, collectively known as The Wizarding World of Harry Potter, have been built at several Universal Parks & Resorts amusement parks around the world.
+    """
+    doc = nlp(text)
+    summary = summarize(doc)
+    print(f"Original Text Length: {len(text)}")
+    print(f"Summary Text Length: {len(summary)}")
+    print(f"Summary: {summary}")
