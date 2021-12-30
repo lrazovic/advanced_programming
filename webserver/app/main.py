c84416de87877856b13f07ac648d3c9c2cf30058
ai18 <antonio.ionta.18@gmail.com>
2021-12-30 10:13:16 +0100

    Implementation of FetcherAPI

--- webserver/app/main.py
+++ webserver/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "Davide"}
@@ -11,3 +10,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id, q = None):
     return {"item_id": item_id, "q": q}
+
+# -----------------------------------------FETCHER MODULE
+import fetcher.main as fetcher
+
+@app.get("/getNews")
+def call_fetcher():
+    return fetcher.retrive_information()
