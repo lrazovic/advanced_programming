04367e16e140f71208b64129ec40a6f94885007b
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-01-14 12:40:56 +0100

    Use async/await syntax to perform REST operations

--- view/helpers/api.js
+++ view/helpers/api.js
@@ -1,49 +1,52 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3000/api/'
 
-export function post(_this, url, payload, successCallback, errorCallback) {
+export async function post(_this, url, payload, successCallback, errorCallback) {
   const headers = ''
 
-  return axios({
-    method: 'POST',
-    url: url,
-    data: payload,
-    headers: headers
-  }).then(response => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: url,
+      data: payload,
+      headers: headers
+    })
     successCallback(response)
-  }).catch(error => {
-    if (!error.status) { console.log('network error') }
+  } catch (error) {
+    if (!error.status) { console.log('network error')} 
     console.log(error.response)
-    if (errorCallback) { errorCallback(error) }
-  })
+    if (errorCallback) { errorCallback(error)} 
+  }
 }
 
-export function get(_this, url, payload, successCallback, errorCallback) {
+export async function get(_this, url, payload, successCallback, errorCallback) {
   const headers = ''
 
-  return axios({
-    method: 'GET',
-    url: url,
-    params: payload.params,
-    headers: headers
-  }).then(response => {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: url,
+      params: payload.params,
+      headers: headers
+    })
     successCallback(response)
-  }).catch(error => {
-    if (errorCallback) { errorCallback(error) }
-  })
+  } catch (error) {
+    if (errorCallback) { errorCallback(error)} 
+  }
 }
 
-export function del(_this, url, payload, successCallback, errorCallback) {
+export async function del(_this, url, payload, successCallback, errorCallback) {
   const headers = ''
 
-  return axios({
-    method: 'DELETE',
-    url: url,
-    headers: headers,
-    payload: payload
-  }).then(response => {
+  try {
+    const response = await axios({
+      method: 'DELETE',
+      url: url,
+      headers: headers,
+      payload: payload
+    })
     successCallback(response)
-  }).catch(error => {
-    if (errorCallback) { errorCallback(error) }
-  })
+  } catch (error) {
+    if (errorCallback) { errorCallback(error)} 
+  }
 }
