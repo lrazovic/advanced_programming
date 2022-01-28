7aa3bf836019e0aaab2eaba5a192e0ce03f27c0e
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-01-28 23:01:15 +0100

    Remove # from url

--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return savedPosition || { top: 0 }
