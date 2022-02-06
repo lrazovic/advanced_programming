8d321bf6927883a1c2a819cac0f097fe772b3d57
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-06 23:47:15 +0100

    Remove fetcher experiments

--- fetcher/app/main.py
+++ fetcher/app/main.py
@@ -1,79 +1,9 @@
 import feedparser
+import logging
 from jsonrpcserver import method, Success, serve, Error
 from bs4 import BeautifulSoup
 
-import logging
-
 logging.getLogger().setLevel(logging.INFO)
-
-#FIXME
-# Schifoso codice scritto da Antonio
-############################################################################################################################################################
-import psycopg
-import sqlalchemy
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, true
-from sqlalchemy.orm import sessionmaker
-
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'user'
-
-    #TODO Autgeneration id
-    id = Column(Integer, primary_key=True)
-    
-    iss = Column(String)
-    azp = Column(String)
-    aud = Column(String)
-    sub = Column(String)
-    email = Column(String)
-    emailVerfied = Column(String)
-    atHash = Column(String)
-    nonce = Column(String)
-    name = Column(String)
-    picture = Column(String)
-    givenName = Column(String)
-    familyName = Column(String)
-    locale = Column(String)
-    iat = Column(String)
-    exp = Column(String)
-    
-    #TODO
-    # def __repr__(self):
-    #     return "<Book(title='{}', author='{}', pages={}, published={})>"\
-    #             .format(self.title, self.author, self.pages, self.published)
-
-def createUserRelation():
-    engine = sqlalchemy.create_engine('postgresql+pg8000://pguser:pguser@localhost:5432/pgdb')
-    print(engine)
-
-    Base.metadata.create_all(engine)
-
-def addUser(dto: User):
-    engine = sqlalchemy.create_engine('postgresql+pg8000://pguser:pguser@localhost:5432/pgdb')
-
-    Session = sessionmaker(bind=engine)
-    s = Session()
-
-    s.add(dto)
-    s.commit()
-
-    s.close()
-
-    return true
-    
-@method
-def crud_test(dto: User):
-    logging.info(f"DTO received: {dto}")
-    r = addUser(dto)
-    if r:
-        return Success()
-    else:
-        return Error()
-############################################################################################################################################################
 
 
 def get_posts_details(rss=None, last=10):
