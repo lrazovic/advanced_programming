5249d0b2cc1e2c2673b4ca098b2740cb48668328
nurper <somedvper@gmail.com>
2022-02-16 23:49:24 +0100

    toggle fix for width

--- view/src/components/NavBar.vue
+++ view/src/components/NavBar.vue
@@ -2,22 +2,23 @@
 import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 import {
+  mdiForwardburger,
+  mdiBackburger,
   mdiClose,
   mdiDotsVertical,
-  mdiAccount,
-  mdiCogOutline,
-  mdiEmail,
+  mdiMenu,
   mdiLogout,
   mdiThemeLightDark
 } from '@mdi/js'
 import NavBarItem from '@/components/NavBarItem.vue'
 import NavBarItemLabel from '@/components/NavBarItemLabel.vue'
 import NavBarMenu from '@/components/NavBarMenu.vue'
-import NavBarMenuDivider from '@/components/NavBarMenuDivider.vue'
-import UserAvatar from '@/components/UserAvatar.vue'
 import Icon from '@/components/Icon.vue'
+// import NavBarSearch from '@/components/NavBarSearch.vue'
 
 const store = useStore()
+
+const lightBorderStyle = computed(() => store.state.lightBorderStyle)
 
 const toggleLightDark = () => {
   store.dispatch('darkMode')
@@ -29,6 +30,10 @@ const isAsideMobileExpanded = computed(() => store.state.isAsideMobileExpanded)
 
 const userName = computed(() => store.state.userName)
 
+const menuToggleMobileIcon = computed(() => isAsideMobileExpanded.value ? mdiBackburger : mdiForwardburger)
+
+const menuToggleMobile = () => store.dispatch('asideMobileToggle')
+
 const isMenuNavBarActive = ref(false)
 
 const menuNavBarToggleIcon = computed(() => isMenuNavBarActive.value ? mdiClose : mdiDotsVertical)
@@ -37,15 +42,41 @@ const menuNavBarToggle = () => {
   isMenuNavBarActive.value = !isMenuNavBarActive.value
 }
 
+const menuOpenLg = () => {
+  store.dispatch('asideLgToggle', true)
+}
 </script>
 
 <template>
   <nav
     v-show="isNavBarVisible"
-    class="top-0 left-0 right-0 fixed flex bg-white h-14 border-b border-gray-100 z-30 w-screen
+    class="top-0 left-0 right-0 fixed flex bg-white h-14 border-b z-30 w-screen
     transition-position xl:pl-60 lg:w-auto lg:items-stretch dark:bg-gray-900 dark:border-gray-800"
-    :class="{'ml-60 lg:ml-0':isAsideMobileExpanded}"
+    :class="[lightBorderStyle, {'ml-60 lg:ml-0':isAsideMobileExpanded}]"
   >
+    <div class="flex-1 items-stretch flex h-14">
+      <nav-bar-item
+        type="flex lg:hidden"
+        @click.prevent="menuToggleMobile"
+      >
+        <icon
+          :path="menuToggleMobileIcon"
+          size="24"
+        />
+      </nav-bar-item>
+      <nav-bar-item
+        type="hidden lg:flex xl:hidden"
+        @click.prevent="menuOpenLg"
+      >
+        <icon
+          :path="mdiMenu"
+          size="24"
+        />
+      </nav-bar-item>
+<!--      <nav-bar-item>-->
+<!--        <nav-bar-search />-->
+<!--      </nav-bar-item>-->
+    </div>
     <div class="flex-none items-stretch flex h-14 lg:hidden">
       <nav-bar-item @click.prevent="menuNavBarToggle">
         <icon
@@ -62,39 +93,8 @@ const menuNavBarToggle = () => {
       <div
         class="max-h-screen-menu overflow-y-auto lg:overflow-visible lg:flex lg:items-stretch lg:justify-end lg:ml-auto"
       >
-        <nav-bar-menu has-divider @click="$router.push('/profile')">
-<!--          <user-avatar class="w-6 h-6 mr-3 inline-flex" />-->
-          <div>
-            <span>{{ userName }}</span>
-          </div>
-
-<!--          <template #dropdown>-->
-<!--            <nav-bar-item to="/profile">-->
-<!--              <nav-bar-item-label-->
-<!--                :icon="mdiAccount"-->
-<!--                label="My Profile"-->
-<!--              />-->
-<!--            </nav-bar-item>-->
-<!--            <nav-bar-item>-->
-<!--              <nav-bar-item-label-->
-<!--                :icon="mdiCogOutline"-->
-<!--                label="Settings"-->
-<!--              />-->
-<!--            </nav-bar-item>-->
-<!--            <nav-bar-item>-->
-<!--              <nav-bar-item-label-->
-<!--                :icon="mdiEmail"-->
-<!--                label="Messages"-->
-<!--              />-->
-<!--            </nav-bar-item>-->
-<!--            <nav-bar-menu-divider />-->
-<!--            <nav-bar-item>-->
-<!--              <nav-bar-item-label-->
-<!--                :icon="mdiLogout"-->
-<!--                label="Log Out"-->
-<!--              />-->
-<!--            </nav-bar-item>-->
-<!--          </template>-->
+        <nav-bar-menu @click="$router.push('/profile')" has-divider>
+          {{ userName }}
         </nav-bar-menu>
         <nav-bar-item
           has-divider
