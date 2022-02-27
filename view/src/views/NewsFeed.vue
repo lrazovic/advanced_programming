49ea5122f1be0aea7d3b5f30c7e444fb0cda8bce
nurper <somedvper@gmail.com>
2022-02-28 00:19:19 +0100

    fix newsfeed

--- view/src/views/NewsFeed.vue
+++ view/src/views/NewsFeed.vue
@@ -23,7 +23,7 @@
   </section>
   <section class="bg-white border-t border-b border-gray-100 p-6 mt-3 dark:bg-gray-900 dark:border-gray-900 dark:text-white">
     <level>
-      <h1 @click="$router.push('source-list')" class="text-3xl font-semibold leading-tight">
+      <h1 @click="$router.push('source-list')" class="text-3xl font-semibold leading-tight cursor-pointer">
         {{ main_title ? main_title : "News sources are empty. Click to add new sources" }}
       </h1>
     </level>
